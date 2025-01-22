1252248fadc5fdc24a015800b61a9bbb0a29c802
phicae4f <mila.masterova@gmail.com>
2025-01-22 19:35:00 +0300

    before content

--- src/components/Layout/AppHeader.jsx
+++ src/components/Layout/AppHeader.jsx
@@ -8,7 +8,7 @@ export default function AppHeader() {
   const [select, setSelect] = useState(false);
   const [modal, setModal] = useState(false);
   const [coin, setCoin] = useState(null);
-  const [drawer, setDrawer] = useState(true);
+  const [drawer, setDrawer] = useState(false);
   const { crypto } = useCrypto();
 
   useEffect(() => {
